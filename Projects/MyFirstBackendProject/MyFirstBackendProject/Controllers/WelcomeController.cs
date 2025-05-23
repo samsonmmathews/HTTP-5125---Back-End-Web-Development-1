f211abf7b5036d195521353f790cd57b41348c6c
Samson Mathews <samsonmmathews@gmail.com>
2025-05-23 00:09:00 -0400

    HTTP-5125 - Back-End Web Development 1 files

---
+++ Projects/MyFirstBackendProject/MyFirstBackendProject/Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstBackendProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        // GET localhost:xx/api/Welcome -> "Hello 5125!"
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello 5125!";
+        }
+    }
+}
