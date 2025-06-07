6622808af21380111b035e10952585cea1c1d0b8
Samson Mathews <samsonmmathews@gmail.com>
2025-06-06 20:52:22 -0400

    Assignment files before changes

---
+++ Projects/Assignment_01/Assignment_01/Controllers/Quest01Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Quest01Controller : ControllerBase
+    {
+        // <summary>
+        //    This will return a Welcome message
+        // </summary>
+
+        [HttpGet(template: "Welcome")]
+        public string Welcome() => "Welcome to 5125!";
+    }
+}
---
+++ Projects/Assignment_01/Assignment_01/Controllers/Quest02Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Quest02Controller : ControllerBase
+    {
+    }
+}
